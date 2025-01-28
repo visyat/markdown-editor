3cc599a4c11f02a39c66da86261c2bef4d9f27ae
Vishal Yathish <vishal.yathish@gmail.com>
2025-01-28 12:35:34 -0800

    added custom keyboard shortcuts and handling for newlines

--- index.js
+++ index.js
@@ -6,10 +6,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updatePreview() {
         const content = editor.value;
-        // console.log('Updating preview with:', content);
-        render.innerHTML = marked.parse(content);
+        const newlines = content.replace(/\n/g, '\\\n');
+        // need to add specific handling of newlines for bullet point lists ...
+        render.innerHTML = marked.parse(newlines);
     }
 
     editor.addEventListener('input', updatePreview);
     updatePreview();
+
+    function addMarkdownFormatting(editor, openTag, closeTag) {
+        // modify this such that if there is text selected, place the tags around instead of totally overwriting ...
+        const start = editor.selectionStart;
+        const end = editor.selectionEnd;
+        const content = editor.value;
+        
+        editor.value = content.slice(0, start) + openTag + closeTag + content.slice(end);
+        editor.selectionStart = start + openTag.length;
+        editor.selectionEnd = start + openTag.length;
+    }
+    editor.addEventListener('keydown', function(e) {
+        const cmdOrCtrl = e.metaKey || e.ctrlKey;
+        if (cmdOrCtrl && e.shiftKey && e.key.toLowerCase() === 'x') {
+            e.preventDefault();
+            addMarkdownFormatting(this, '~~', '~~');
+        }
+        if (cmdOrCtrl && e.key.toLowerCase() === 'b') {
+            e.preventDefault();
+            addMarkdownFormatting(this, '**', '**');
+        }
+        if (cmdOrCtrl && e.key.toLowerCase() === 'i') {
+            e.preventDefault();
+            addMarkdownFormatting(this, '*', '*');
+        }
+        if (cmdOrCtrl && e.key.toLowerCase() === 'u') {
+            e.preventDefault();
+            addMarkdownFormatting(this, '<u>', '</u>');
+        }
+        if (e.key === 'Tab') {
+            e.preventDefault();
+            const start = this.selectionStart;
+            const content = this.value;
+            this.value = content.slice(0, start) + '    ' + content.slice(start);
+            this.selectionStart = this.selectionEnd = start + 4;
+        }
+    });
+
+    editor.addEventListener('paste', function(e) {
+        const dT = e.clipboardData || window.clipboardData;
+        const file = dT.files[ 0 ];
+        console.log( file );
+        // save image file to directory where markdown notes are stored ...
+    });
 });
